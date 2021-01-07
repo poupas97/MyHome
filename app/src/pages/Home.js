ca9d649173feadba253fde31e7b7f300bc97502b
Rúben Carreira <ruben_carreira97@hotmail.com>
2021-01-07 20:03:31 +0000

    deploy 2

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export const HOME_ROUTE = '/';
 
-const Home = () => (
-  <h3>
-    Hello, My Home
-  </h3>
-);
+const Home = () => {
+  const history = useHistory();
+
+  return (
+    <>
+      <h3>
+        Hello, My Home
+      </h3>
+      <button onClick={() => history.push('/sensors')}>Sensors List</button>
+    </>
+  );
+};
 
 export default Home;
